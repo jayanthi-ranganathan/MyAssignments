ce88e2042542a0fa4f43f3b19ffc1497f3307a07
jayanthi-ranganathan <jai.ranga@gmail.com>
2026-03-01 12:36:29 +0530

    Add files via upload

---
+++ assignmentLet.js
@@ -0,0 +1,11 @@
+
+let userName = "Jayanthi";
+let organization = "TestLeaf";  
+let contactNumber = 9865678766;              
+let knowsAutomation = true;               
+let usesPlaywright;                       
+console.log(typeof userName);
+console.log(typeof organization);
+console.log(typeof contactNumber);
+console.log(typeof knowsAutomation);
+console.log(typeof usesPlaywright);
